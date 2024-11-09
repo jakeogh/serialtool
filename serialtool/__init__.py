a1baf897e2fa76dc7dbfb09f81c8ad34f3517a57
jakeogh <github.com@v6y.net>
2024-11-09 00:03:13 -0700

    auto-commit

--- serialtool/__init__.py
+++ serialtool/__init__.py
@@ -1,5 +1,9 @@
-from .serialtool import SerialOracle
-from .serialtool import construct_serial_command
-from .serialtool import generate_serial_port_help
-from .serialtool import print_serial_oracle
-from .serialtool import print_serial_output
+"""
+isort:skip_file
+"""
+
+from .serialtool import SerialOracle as SerialOracle
+from .serialtool import construct_serial_command as construct_serial_command
+from .serialtool import generate_serial_port_help as generate_serial_port_help
+from .serialtool import print_serial_oracle as print_serial_oracle
+from .serialtool import print_serial_output as print_serial_output
