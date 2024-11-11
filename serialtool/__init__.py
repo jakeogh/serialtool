daa290039283835984c49667606ab79936f92093
jakeogh <github.com@v6y.net>
2024-11-11 01:13:16 -0700

    auto-commit

--- serialtool/__init__.py
+++ serialtool/__init__.py
@@ -7,3 +7,4 @@ from .serialtool import construct_serial_command as construct_serial_command
 from .serialtool import generate_serial_port_help as generate_serial_port_help
 from .serialtool import print_serial_oracle as print_serial_oracle
 from .serialtool import print_serial_output as print_serial_output
+from .serialtool import SerialNoResponseError as SerialNoResponseError
