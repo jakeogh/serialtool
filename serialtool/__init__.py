44cb97e236cd3b18c3043f6fc747e284c9d89ca3
jakeogh <github.com@v6y.net>
2025-11-09 23:03:35 -0700

    auto-commit

--- serialtool/__init__.py
+++ serialtool/__init__.py
@@ -9,5 +9,5 @@ from .serialtool import construct_serial_command_ack as construct_serial_command
 from .serialtool import generate_serial_port_help as generate_serial_port_help
 from .serialtool import print_serial_output as print_serial_output
 
-# from .serialtool import print_serial_oracle as print_serial_oracle
+from .serialtool import print_serial_oracle as print_serial_oracle
 from .serialtool import SerialNoResponseError as SerialNoResponseError
